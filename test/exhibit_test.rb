7d83242fd9416506705665c12aa0186ed2e89bba
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-07 11:20:00 -0500

    Begin exhibit feature

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -7,7 +7,7 @@ class ExhibitTest < Minitest::Test
   def test_it_exists_and_has_readable_attributes
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    assert_instance_of Exihibit, exhibit
+    assert_instance_of Exhibit, exhibit
     assert_equal "Gems and Minerals", exhibit.name
   end
 
